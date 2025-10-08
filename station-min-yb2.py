0686cc33addcf4419e0530b6818b5a8b75e91b80
potatosserver <potatosserver@gmail.com>
2025-10-08 09:08:01 +0800

    Update station-min-yb2.py

--- station-min-yb2.py
+++ station-min-yb2.py
@@ -1,11 +1,9 @@
 import requests
+import json
 
 headers = {
     'accept': '*/*',
     'accept-language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6',
-    'content-type': 'application/x-www-form-urlencoded',
-    'if-modified-since': 'Sat, 03 May 2025 09:40:26 GMT',
-    'if-none-match': 'W/"6815e48a-358846"',
     'origin': 'https://www.youbike.com.tw',
     'priority': 'u=1, i',
     'referer': 'https://www.youbike.com.tw/',
@@ -18,5 +16,12 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36',
 }
 
-response = requests.get('https://apis.youbike.com.tw/json/station-min-yb2.json', headers=headers)
-response.json()
+url = 'https://apis.youbike.com.tw/json/station-min-yb2.json'
+
+try:
+    response = requests.get(url, timeout=10)
+    data = response.json()
+    print(json.dumps(data, indent=2, ensure_ascii=False))
+
+except Exception as e:
+    print(f"發生未預期的錯誤: {e}")
